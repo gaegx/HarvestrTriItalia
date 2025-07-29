cd51dab1a0de1fee29e94cc87a14a2f1e22a648e
danil <danilaohremcuk@gmail.com>
2025-07-29 12:17:29 +0300

    refactor

--- src/main/java/com/gaegxh/harvester/model/Criteria.java
+++ src/main/java/com/gaegxh/harvester/model/Criteria.java
@@ -6,11 +6,6 @@ import lombok.Data;
 @Data
 @Builder
 public class Criteria {
-    private boolean frecceOnly;
-    private boolean regionalOnly;
-    private boolean intercityOnly;
-    private boolean tourismOnly;
-    private boolean noChanges;
     private String order;
     private int offset;
     private int limit;
